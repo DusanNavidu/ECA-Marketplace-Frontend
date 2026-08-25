b4b1b9a2a9dfd70930ec3a3113d73f0a749a6a39
navidu-cora-labs <navidu.coralabs@gmail.com>
2026-08-26 05:20:16 +0530

    Update API Base URL to Cloud API Gateway

--- src/api/axios.js
+++ src/api/axios.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 const instance = axios.create({
-    baseURL: 'http://localhost:8080',
+    baseURL: 'http://35.232.99.31:8080',
 });
 
 instance.interceptors.request.use((config) => {
